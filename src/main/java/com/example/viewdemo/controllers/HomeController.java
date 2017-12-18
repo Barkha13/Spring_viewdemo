4574fe9d023c1300d517d47468b6dcd2e2e54d36
Barkha13 <barkha0292@yahoo.com>
2017-12-18 00:51:45 -0800

    added code to pass data to template

--- src/main/java/com/example/viewdemo/controllers/HomeController.java
+++ src/main/java/com/example/viewdemo/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package com.example.viewdemo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
 //		return "forward:/index.html";
 //		return "index.jsp";
+		model.addAttribute("name","Bunny");
 		return "index";
 	}
 
