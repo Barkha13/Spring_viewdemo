07fa4fe8325821279d448b7c8eb761a7095f607c
Barkha13 <barkha0292@yahoo.com>
2017-12-18 00:34:58 -0800

    first commit

---
+++ src/main/java/com/example/viewdemo/ViewdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.viewdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewdemoApplication.class, args);
+	}
+}
